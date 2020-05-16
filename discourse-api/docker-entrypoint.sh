a202991338c0c57e88eae77f8cd7731fbd7c9f86
tomasfse <tomasferser+github@gmail.com>
2020-05-16 17:43:31 +0200

    Plugins installation support

--- discourse-api/docker-entrypoint.sh
+++ discourse-api/docker-entrypoint.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
-set -ex
-sh ${DISCOURSE_OPT_PATH}/scripts/copy-env.sh
-echo $@
-if [ $1 == "sidekiq" ]; then
-    bundle exec sidekiq -q critical -q default -q low -v
-elif [ $1 == "server" ]; then
-    bundle exec rake db:migrate
-    #If persisted assets path is empty...
-    if [ -z "$(ls -A ${DISCOURSE_WWW_PATH}/public/assets)" ]; then
-    echo "Assets path is empty. 'assets:precomplie' required"
-    bundle exec rake assets:precompile
+
+function main {
+    sh ${DISCOURSE_OPT_PATH}/scripts/copy-env.sh
+    if [ "$1" == "sidekiq" ]; then
+        bundle exec sidekiq -q critical -q default -q low -v
+    elif [ "$1" == "server" ]; then
+        
+        # Installing Plugins
+        cd ${DISCOURSE_WWW_PATH}/plugins
+        discourse_plugins_list=($(echo $DISCOURSE_PLUGINS | tr "," "\n"))
+        for plugin in "${discourse_plugins_list[@]}"; do
+            git clone --single-branch --depth 1 $plugin
+        done
+        cd -
+
+        # Starting DB migrations
+        bundle exec rake db:migrate
+
+        # If persisted assets path is empty...
+        if [ -z "$(ls -A ${DISCOURSE_WWW_PATH}/public/assets)" ]; then
+        echo "Assets path is empty. 'assets:precomplie' required"
+        bundle exec rake assets:precompile
+        else
+            echo "Assets path has content. 'assets:precomplie' skipped"
+        fi
+
+        # Running server
+        bundle exec rails server -b 0.0.0.0
     else
-        echo "Assets path has content. 'assets:precomplie' skipped"
-    fi
-    bundle exec rails server -b 0.0.0.0
-else
-    exec "$@"
-fi
+        exec "$@"
+    fi 
+}
+
+set -ex
+main $@ | ts
